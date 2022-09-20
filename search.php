42565f1a0a6fb737527d0dc2711d01da5fb7dedc
Joel Calado <joelcalado@gmail.com>
2022-09-20 15:23:12 +0100

    Fix the displayed value on the search page

--- search.php
+++ search.php
@@ -10,6 +10,12 @@ if (get_locale() == "pt_BR") {
   $cx = "006860825493368957871:dhhtt8i5dmk";
 }
 
+if (isset($_GET['q'])) {
+  $search = htmlspecialchars($_GET['q']);
+} else {
+  $search = '';
+}
+
 require(get_template_directory() . '/components/parent/header.php');
 ?>
 
@@ -18,10 +24,10 @@ require(get_template_directory() . '/components/parent/header.php');
     <div class="row">
       <div class="col">
         <header class="my-5">
-          <h1 class="h2"><?= _e('Results for:', 'iasd'); ?> <b><?php echo htmlspecialchars(isset($_GET['q'])); ?></b></h1>
+          <h1 class="h2"><?= _e('Results for:', 'iasd'); ?> <b><?php echo $search; ?></b></h1>
           <form method="get" action="<?php echo site_url(); ?>/busca/?" class="search_form">
             <div class="input-group mt-4">
-              <input type="text" name="q" class="form-control" value="<?php echo htmlspecialchars(isset($_GET['q'])); ?>">
+              <input type="text" name="q" class="form-control" value="<?php echo $search; ?>">
               <span class="input-group-btn">
                 <button class="btn btn-default" type="submit"><?= _e('Search', 'iasd'); ?></button>
               </span>
