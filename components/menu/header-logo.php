18e2598f2d0257537e639cbf80977376a84b98e6
Joel Calado <joelcalado@gmail.com>
2024-11-19 19:28:08 +0000

    always link to adventistas.org.pt

--- components/menu/header-logo.php
+++ components/menu/header-logo.php
@@ -3,10 +3,7 @@
 <?php $default_logo = isset($sede['ct_headquarter']->slug) ? get_template_directory_uri() . "/assets/sedes/" . LANG . "/" . $sede['ct_headquarter']->slug . ".svg" : '' ?>
 
 <div class="col-auto d-flex align-items-center pa-header-logo">
-  <a href="<?= get_home_url(); ?>" class="w-auto align-middle">
-    <img src="<?= $custom_logo ? $custom_logo[0] : $default_logo ?>" 
-         alt="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" 
-         title="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" 
-         class="h-100 w-auto pa-logo-iasd">
+  <a href="https://www.adventistas.org.pt" class="w-auto h-100">
+    <img src="<?= get_template_directory_uri() . "/assets/sedes/" . LANG . "/" . $sede['ct_headquarter']->slug . ".svg" ?>" alt="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" title="<?= !empty($sede['ct_headquarter']->name) ? $sede['ct_headquarter']->name : '' ?>" class="h-100 w-auto pa-logo-iasd">
   </a>
 </div>
